dc8f5aebdd6b3d7a07e738e8d249d0046207e389
Anmol <anmolpatial61@gmail.com>
2025-04-21 12:36:53 +0530

    21/04/25 commit-2

--- JAVASCRIPT/function.js
+++ JAVASCRIPT/function.js
@@ -1,21 +1,39 @@
+// console.log("Start");
+
+// var a = 10;
+// var b = 20; 
+
+// function addition1() {
+//     var a =1;
+//     var b = 2;
+//     return a+b;
+// }
+
+// function addition2() {
+//     var a = 30;
+//     var b = 40;
+//     return a+b;
+// }
+
+// console.log(addition1());
+// console.log(addition2());
+
+// console.log("End");
+
+// ? Anonymous Functions
 console.log("Start");
 
-var a = 10;
-var b = 20; 
+console.log(sayHello); // entire function
 
-function addition1() {
-    var a =1;
-    var b = 2;
-    return a+b;
+function sayHello() {
+    return "Hii I am from named function";
 }
 
-function addition2() {
-    var a = 30;
-    var b = 40;
-    return a+b;
-}
+console.log(sayHello);  // entire function
 
-console.log(addition1());
-console.log(addition2());
+// --------------------------------------
 
-console.log("End");
+let greet = function () {
+    return "Hello how are you";
+};
+console.log(greet); // entire function
