b520ff2b305ce5b7d1583cd85a3810417659ab7d
Anmol <anmolpatial61@gmail.com>
2025-04-21 12:14:04 +0530

    21/4/25 p1

---
+++ JAVASCRIPT/function.js
@@ -0,0 +1,21 @@
+console.log("Start");
+
+var a = 10;
+var b = 20; 
+
+function addition1() {
+    var a =1;
+    var b = 2;
+    return a+b;
+}
+
+function addition2() {
+    var a = 30;
+    var b = 40;
+    return a+b;
+}
+
+console.log(addition1());
+console.log(addition2());
+
+console.log("End");
